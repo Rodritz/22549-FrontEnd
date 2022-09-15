2ea6014529e2045f270e3a27408e5ebb35d1184c
Rodritz <rodrigourq@gmail.com>
2022-09-15 20:11:23 -0300

    cambios

---
+++ Clase 13/js/operaciones.js
@@ -0,0 +1,19 @@
+//comentario en js//
+/*comentario en js*/
+
+function saludar() { 
+    alert('Hola mundo dentro de saludar');
+}
+
+/*operadores aritmeticos*/
+
+//variable: espacio de memoria//
+//sumar dos variables y mostrar su resultado//
+let op1= prompt ('ingrese el valor de op1');
+let op2= prompt ('ingrese el valor de op2');
+let res; //declaro la variable
+res= (+op1)+ parseInt(op2);
+
+//console.log (res);//muestro el resultado por consola
+
+alert('resultado=' +res); //muestra alerta
