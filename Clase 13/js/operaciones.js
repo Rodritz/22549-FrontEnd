ce1e9fd4821e896e4bf59c4a72a00bd95dad5fe5
Rodritz <rodrigourq@gmail.com>
2022-09-25 17:56:18 -0300

    cambios

--- Clase 13/js/operaciones.js
+++ Clase 13/js/operaciones.js
@@ -9,12 +9,13 @@ function saludar() {
 
 //variable: espacio de memoria//
 //sumar dos variables y mostrar su resultado//
-let op1= prompt ('ingrese el valor de op1');
+let op1= prompt ('ingrese el valor de op1');//la funcion promt me devuelve un string
 let op2= prompt ('ingrese el valor de op2');
 let res; //declaro la variable
 res= (+op1)+ parseInt(op2);
-
+//tambien podria haber dicho "let res= op1 + op2"
 
 console.log (res);//muestro el resultado por consola
 
 //alert('resultado=' +res); //muestra alerta
+
