1e1f681acbabbef7f62d84d1a645cb1e360f469a
Rodritz <rodrigourq@gmail.com>
2022-10-06 20:07:21 -0300

    cambios

--- Clase 18/js/script.js
+++ Clase 18/js/script.js
@@ -2,12 +2,21 @@ const btnAdd = document.getElementById('btnAdd');
 //el scope del array debe estar por encima de la funcion add
 const claves = [];
 
+//para asociar a un btn un evento
+btnAdd.addEventListener('click', add); //esto seria el onclick="add()"" en el html
+
+//declaro  la funcion add que llama "btnAdd.addEventListener"
 function add() {
 
+    /*podemos decir
+    const clave = document.getElementById('clave').value;
+    o utilizar una funcion*/
     const clave = getKey();
 
+    //invocamos la funcion booleana para validacion
     const isValid = validateKey(clave);
 
+    //si la validacion da falso, entonces no pasa la validacion
     if (!isValid) {
         alert('Debe completar la clave');
         return;
@@ -23,14 +32,15 @@ function add() {
 
 function renderKeyByDOM() {
     const list = findKeys();
-
     //crear un <ul>
+    
     const ul = document.createElement('ul');
     ul.id = 'clavesUl';
     ul.className = 'classUl';
 
     //agregar al div el hijo: ul
     list.appendChild(ul);
+
 
     //crear los <li> en base a el array de clave y los agrego
     //al <ul>
@@ -45,33 +55,20 @@ function renderKeyByDOM() {
         ul.appendChild(li);
     };
 }
-
-//---------------
-
-/*function renderKeys() {
-    const list = findKeys();
-    list.innerHTML = '';
-    claves.forEach(clave => {
-        list.innerHTML += `<li>${clave}</li>`
-    });
-}*/
-
-//true|false
-function validateKey(key) {
-    //validacion
-    return key !== '';//true | false
-}
-
-function addToList(clave) {
-    claves.push(clave);
-}
-
+//funcion para obtener la clave ingresada
 function getKey() {
     return document.getElementById('clave').value;
+}
+
+//true|false para validacion
+function validateKey(clave) {
+    return clave !== '';//true | false
+}
+//agregar a la lista de claves
+function addToList(clave) {
+    claves.push(clave);
 }
 
 function findKeys() {
     return document.getElementById('claves');
 }
-
-btnAdd.addEventListener('click', add);
