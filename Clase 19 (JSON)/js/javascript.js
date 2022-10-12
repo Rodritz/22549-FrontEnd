855d607716b9fe4e40f35bfa403e78858595ef32
Rodritz <rodrigourq@gmail.com>
2022-10-12 20:21:50 -0300

    cambios

--- Clase 19 (JSON)/js/javascript.js
+++ Clase 19 (JSON)/js/javascript.js
@@ -29,17 +29,20 @@ function buscarDatos() {
     return persona;
 }
 
+
 function dibujarDatosPersona() {
 
     //obtengo los datos desde "api rest"
     const res = buscarDatos();
 
-    const datosPersona = DatosPersona(res.dni, res.nombre);
+    const datosPersona = DatosPersona(res.dni, res.nombre); //llamamos a la primer funcion
 
     const direcciones = DireccionesPersona(res.direcciones);
 
     const cuenta = DatosCuentaPersona(res.cuenta);
 
+    /*creamos una funcion render para crear un objeto, 
+    y le pasamos los parametros que va a devolver*/
     render({
         datosPersona: datosPersona,
         direcciones: direcciones,
@@ -47,12 +50,8 @@ function dibujarDatosPersona() {
     });
 }
 
-function render(props) {
-    document.getElementById('datos-persona').innerHTML = props.datosPersona;
-    document.getElementById('direcciones-persona').innerHTML = props.direcciones;
-    document.getElementById('cuenta-persona').innerHTML = props.cuenta;
-}
-
+/*la funcion DatosPersona va a recibir el objeto 
+que necesito, en este caso dni y nombre*/
 function DatosPersona(dni, nombre) {
     const html = `<p>${nombre} - ${dni}</p>`;
     return html;
@@ -64,13 +63,13 @@ function DatosPersona(dni, nombre) {
 </ul>
 */
 function DireccionesPersona(direcciones) {    
-    let html = '<ul>';//alt+96
+    let html = '<ul>';//alt+96 para las comillas invertidas, que me permite concatenar variables en un string
     const dirs = direcciones.map(d => `<li>${d.calle},${d.altura},${d.tipo}</li>`);
-    html += dirs.join(''); //join sirve para unir los string
-    //for!
+    html += dirs.join(''); //array.join sirve para unir los string
+    //for! 
     //forEarch
-    //join
-    html += '</ul>';
+    //join es un metodo de los arrays
+    html += '</ul>'; //cerramos el <ul>
     return html;
 }
 
@@ -79,5 +78,45 @@ function DatosCuentaPersona(cuenta) {
         <p>Nro: ${cuenta.nro}</p>`;
     return html;
 }
+
+function render(props) {
+    document.getElementById('datos-persona').innerHTML = props.datosPersona;
+    document.getElementById('direcciones-persona').innerHTML = props.direcciones;
+    document.getElementById('cuenta-persona').innerHTML = props.cuenta;
+}
+
+/////////////////////////
+
+const persona = {
+    dni: 1234,
+    nombre: 'carlos',
+    direcciones: [
+        {
+            calle: 'calle1',
+            altura: 1234,
+            tipo: 'postal'
+        },
+        {
+            calle: 'calle2',
+            altura: 1235,
+            tipo: 'legal'
+        }
+    ],
+    cuenta: {
+        tipo: 'cc',
+        nro: '012-132132-21321'
+    }
+};
+
+console.log(persona);
+
+//con JSON.stringify transformo un objeto a texto
+const personaJsonText = JSON.stringify(persona);
+console.log(personaJsonText);
+
+//con JSON.parse transformo un texto a objeto
+const otraPersona =JSON.parse(personaJsonText);
+console.log(otraPersona);
+
 
 
