83e5a756000a3a6d7f7cc3cf843fb377004e59fb
Rodritz <rodrigourq@gmail.com>
2022-10-18 20:18:46 -0300

    cambios

--- Clase 20/index.js
+++ Clase 20/index.js
@@ -1,28 +1,44 @@
 //invoco la funcion inicial
 render();
 
+function buscar(clave) {
+    // alert(`buscando algo en index.js: ${clave}`);
+
+    //data.js > USER_DATA.data
+    //funciones flecha / arrow function
+    const usersFiltered = USERS_DATA.data
+    .filter(user => user.first_name.toLowerCase().includes(clave.toLowerCase()) || user.last_name.toLowerCase().includes(clave.toLowerCase()));
+
+    const htmlUsers = Users(usersFiltered);
+    document.getElementById('users').innerHTML = htmlUsers;
+}
+
 //disparar a las demas funciones
 function render() {
     console.log('render()');
 
-    //invoco al componente Navbar
-    const htmlNavbar = Navbar('Mi primer pseudocomponente',buscar); //a la funcion navbar le paso una funcion por parametro
-    document.getElementById('navbar').innerHTML = htmlNavbar;
-
-    //ahora invoco a Users
-    const htmlUsers = Users(USERS_DATA.data);
-    document.getElementById('users').innerHTML = htmlUsers;
+    //usar al componente Navbar
+    const htmlNavbar = Navbar('Mi primer pseudocomponente',buscar);
+    renderComponent('navbar',htmlNavbar);
+ 
+    search('https://reqres.in/api/users');
 }
 
-function buscar(clave) {
-    // alert(`buscando algo en index.js: ${clave}`);
+function search(url) {
+    //ahora invoco a 
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const htmlUsers = Users(data.data);
+            renderComponent('users',htmlUsers);
+            
+            //ahora "cargo" el componente Paginator()
+            const htmlPaginator = Paginator(data);
+            renderComponent('paginator',htmlPaginator);
+        }
+    );
+}
 
-    /*la const usersFiltered va a filtrar el vector
-    buscando la palabra clave. una vez encontrada, la const 
-    htmlUsers equivale a Users(usersFiltered) y la tabla se va a dibujar
-    con estos nuevos datos  */
-    const usersFiltered = USERS_DATA.data.filter(user => user.first_name.includes(clave) || user.last_name.includes(clave));
-
-    const htmlUsers = Users(usersFiltered);
-    document.getElementById('users').innerHTML = htmlUsers;
+function renderComponent(id, htmlComponent) {
+    document.getElementById(id).innerHTML = htmlComponent;
 }
