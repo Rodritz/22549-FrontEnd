8f128cf93be857e3d058ba34fdf5168fed555338
Rodritz <rodrigourq@gmail.com>
2022-10-23 20:28:18 -0300

    cambios

---
+++ trabajo practico/js/ventas.js
@@ -0,0 +1,48 @@
+document.getElementById('btn-borrar').addEventListener('click', borrar);
+document.getElementById('btn-resumen').addEventListener('click', resumen);
+
+function borrar(){
+    document.getElementById('nombre').value='';
+    document.getElementById('apellido').value='';
+    document.getElementById('email').value='';
+    document.getElementById('cantidad').value='';
+    document.getElementById('categoria').value='';
+    document.getElementById('aPagar').value='';
+}
+
+function resumen(){
+    const entrada = 200;
+    let cant = document.getElementById('cantidad').value;
+    
+    let totalEntradas = entrada * cant;
+
+    let porcentaje = Porcentaje();
+    
+    //let saldo = totalEntradas -(totalEntradas * porcentaje / 100);
+    let saldo = descuento(totalEntradas, porcentaje);   
+
+    //let pagoTotal = totalEntradas - descuento;
+    document.getElementById('aPagar').value = 'Total a pagar $' + saldo;
+
+}
+
+function Porcentaje(){
+    let cat = document.getElementById('categoria').value;
+    if(cat === ''){
+        return 0;        
+    } 
+    if(cat === 'estudiante'){
+        return 80;        
+    }    
+    if(cat === 'trainee'){
+        return 50;        
+    } else {
+        return 15;
+    }
+}
+
+function descuento(totalEntradas, porcentaje){
+    return Math.abs(totalEntradas * porcentaje / 100 - totalEntradas)
+    //return totalEntradas - (totalEntradas * porcentaje / 100)
+}
+
